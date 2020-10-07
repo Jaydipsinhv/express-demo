9a3fbf55909083c73d0930b7a73cbfa7526df724
“Jaydipsinh <jaydip.vaghela@gmail.com>
2020-10-07 19:40:32 +0530

    added postman routes on route defination

--- routes/ganister.js
+++ routes/ganister.js
@@ -10,9 +10,11 @@ const router = express.Router();
 
 // get all the available options for provided JSON
 // Normally we get the data from the database query
+// localhost:3000/ganister/options - POSTMAN
 router.get('/options', (req, res) => res.status(200).json(availableOptions));
 
-// get author name based on book id, we are using the params feature of router
+// get BOM object based on user inputs
+// localhost:3000/ganister/bom?tou=tou002&pn=pn002&ja=ja002 - POSTMAN
 router.get('/bom', (req, res) => {
   const {
     an, tou, ja, pn,
